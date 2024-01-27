741482e8dfea926a1d50bea50b15771385be58c1
Sum Bui <buingocsum@gmail.com>
2024-01-28 04:47:56 +0700

    content board

--- src/theme.js
+++ src/theme.js
@@ -4,13 +4,15 @@ import { colors } from '@mui/material'
 
 const APP_BAR_HEIGHT = '58px'
 const BOARD_BAR_HEIGHT = '60px'
-const BOARD_CONTENT_HEIGHT = '58px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
 
 // Create a theme instance.
 const theme = extendTheme({
   trello:{
-    appBarHeight: '58px',
-    boardBarHeight: '60px'
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT
+
   },
   colorSchemes: {
     // light: {
@@ -61,6 +63,13 @@ const theme = extendTheme({
         // })
         root: { fontSize: '0.875rem' }
       }
+    },
+
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.MuiTypography-body1':{ fontSize: '0.875rem' }
+        } }
     },
 
     MuiOutlinedInput: {
