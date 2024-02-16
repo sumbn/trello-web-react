528b45bf4642b6f6083bdcd8e921e7e7a47dbe60
sumbn <sum.bn37k02.1@gmail.com>
2024-02-16 15:17:27 +0700

    toastify

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button'
 import AddIcon from '@mui/icons-material/Add'
 import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 import { useState } from 'react'
+import { toast } from 'react-toastify'
 import TextField from '@mui/material/TextField'
 import CloseIcon from '@mui/icons-material/Close'
 import theme from '~/theme'
@@ -18,7 +19,8 @@ function ListColumns({ columns }) {
   const addNewColumn = () => {
 
     if (!newColumnTitle) {
-      // console.error('please enter')
+      toast.error('please enter')
+
       return
     }
     // console.log(newColumnTitle)
@@ -49,6 +51,83 @@ function ListColumns({ columns }) {
             borderRadius:'6px',
             height:'fit-content',
             bgcolor:'#ffffff3d'
+        {!openNewColumnForm
+          ? <Box onClick={toggleOpenNewColumnForm} sx={{
+            minWidth:'250px',
+            maxWidth:'250px',
+            mx:2,
+            borderRadius:'6px',
+            height:'fit-content',
+            bgcolor:'#ffffff3d'
+
+          }}>
+            <Button
+              startIcon={<AddIcon/>}
+              sx={{
+                color: 'white',
+                width: '100%',
+                justifyContent:'flex-start',
+                pl: 2.5,
+                py: 1
+              }}
+            >Add new column</Button>
+          </Box>
+          : <Box sx = {{
+            minWidth:'250px',
+            maxWidth:'250px',
+            mx:2,
+            p: 1,
+            borderRadius:'6px',
+            height:'fit-content',
+            bgcolor:'#ffffff3d' }}>
+            <TextField
+              label="Enter column title"
+              type="text"
+              size='small'
+              variant='outlined'
+              autoFocus
+              value={newColumnTitle}
+              onChange={(e) => setNewColumnTitle(e.target.value)}
+              sx={{
+                '& label': { color:'white' },
+                '& input': { color:'white' },
+                '& label.Mui-focused': { color:'white' },
+                '& .MuiOutlinedInput-root': {
+                  '& fieldSet': {
+                    borderColor: 'white'
+                  },
+                  '&:hover fieldSet': {
+                    borderColor: 'white'
+                  },
+                  '&.Mui-focused fieldSet': {
+                    borderColor: 'white'
+                  }
+                }
+              }}/>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <Button
+                onClick={ addNewColumn}
+                variant='contained' color='success' size='small'
+                sx={{
+                  boxShadow: 'none',
+                  border:'0.5px solid',
+                  borderColor:(theme) => theme.palette.success.main,
+                  '&:hover': { bgcolor: (theme) => theme.palette.success.main }
+                }}
+              >Add column
+              </Button>
+              <CloseIcon
+                fontSize='small'
+                sx={{
+                  color: 'white',
+                  cursor : 'pointer',
+                  '&:hover': { color: (theme) => theme.palette.warning.light }
+                }}
+                onClick= {toggleOpenNewColumnForm}
+              />
+            </Box>
+          </Box>
+        }
 
           }}>
             <Button
